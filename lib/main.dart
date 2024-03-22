10dc765be2065e536c8b2839c8a0bdcd94744b12
dimashadiwaskita <hadiwaskitadimas@gmail.com>
2024-03-22 09:42:55 +0700

    update kode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ecommerce_widget_lecture/ui/dashboard.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +8,188 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
+      title: 'My Online Store',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.blue,
       ),
       home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  final List<Product> products = [
+    Product(
+        imageUrl:
+            'https://tse4.mm.bing.net/th?id=OIP.Kc2PFfEbRxLQEPjq4bonOwHaHa&pid=Api&P=0&h=220/4x4',
+        name: 'Sepatu santai',
+        price: 100000),
+    Product(
+        imageUrl:
+            'https://tse3.mm.bing.net/th?id=OIP.wSWp2iZ_3ph6ibN9F93-nAHaEl&pid=Api&P=0&h=220/4x4',
+        name: 'Sepatu volly',
+        price: 135000),
+    Product(
+        imageUrl:
+            'https://tse2.mm.bing.net/th?id=OIP.ztrWbGn0si85V2weKbdUVQHaEs&pid=Api&P=0&h=220/4x4',
+        name: 'Sepatu olahraga',
+        price: 300000),
+  ];
+
+  final List<Product> cart = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Online Store'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Handle search button press
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CartScreen(cart: cart)),
+              );
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: products.map((product) {
+            return ProductCard(
+              product: product,
+              onAddToCart: () {
+                setState(() {
+                  cart.add(product);
+                });
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Product added to cart')),
+                );
+              },
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  final VoidCallback onAddToCart;
+
+  const ProductCard({
+    required this.product,
+    required this.onAddToCart,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 150,
+            child: Image.network(
+              product.imageUrl,
+              width: 4,
+              height: 4,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            product.name,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          Text(
+            'Rp. ${product.price.toString()}',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: onAddToCart,
+            child: Text('Add to Cart'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Product {
+  final String imageUrl;
+  final String name;
+  final double price;
+
+  Product({
+    required this.imageUrl,
+    required this.name,
+    required this.price,
+  });
+}
+
+class CartScreen extends StatelessWidget {
+  final List<Product> cart;
+
+  const CartScreen({required this.cart});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Cart'),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: Image.network(
+              cart[index].imageUrl,
+              width: 50,
+              height: 50,
+            ),
+            title: Text(cart[index].name),
+            subtitle: Text('Rp. ${cart[index].price.toString()}'),
+            trailing: ElevatedButton(
+              onPressed: () {
+                // Action saat tombol "Beli Sekarang" ditekan
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Checkout: ${cart[index].name}')),
+                );
+                // Implementasikan logika belanja di sini
+              },
+              child: Text('Beli Sekarang'),
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
-  const Dashboard({super.key});
+  const Dashboard({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,64 +9,102 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: const Text('My Commerce'),
       ),
-      body: Center(child: LayoutBuilder(
+      body: Center(
+        child: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints viewportConstraints) {
-        return SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          physics: const BouncingScrollPhysics(),
-          child: Expanded(
-            child: IntrinsicHeight(
-              child: Column(children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: viewportConstraints.maxWidth,
-                    child: const Image(
-                        image: NetworkImage(
-                            'https://scene7.toyota.eu/is/image/toyotaeurope/aygo-x-exclusive-red-retail:Small-Landscape?ts=1666960236550&resMode=sharp2&op_usm=1.75,0.3,2,0')),
+            return SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: viewportConstraints.maxWidth,
+                      child: Image(
+                        image: const NetworkImage(
+                            'https://scene7.toyota.eu/is/image/toyotaeurope/aygo-x-exclusive-red-retail:Small-Landscape?ts=1666960236550&resMode=sharp2&op_usm=1.75,0.3,2,0'),
+                      ),
+                    ),
                   ),
-                ),
-                const ListProducts(),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: viewportConstraints.maxWidth,
-                    child: const Image(
+                  const ListProducts(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: viewportConstraints.maxWidth,
+                      child: Image(
                         image: NetworkImage(
-                            'https://scene7.toyota.eu/is/image/toyotaeurope/RAV4-2018-NOT-UK-SPEC-10-scaled:Small-Landscape?ts=0&resMode=sharp2&op_usm=1.75,0.3,2,0')),
+                            'https://scene7.toyota.eu/is/image/toyotaeurope/RAV4-2018-NOT-UK-SPEC-10-scaled:Small-Landscape?ts=0&resMode=sharp2&op_usm=1.75,0.3,2,0'),
+                      ),
+                    ),
                   ),
-                ),
-              ]),
-            ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 60.0,
+          color: Colors.blue,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  // Handle search button press
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {
+                  // Handle cart button press
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.person),
+                onPressed: () {
+                  // Handle profile button press
+                },
+              ),
+            ],
           ),
-        );
-      })),
+        ),
+      ),
     );
   }
 }
 
 class ListProducts extends StatelessWidget {
   const ListProducts({
-    super.key,
+    Key? key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(top: 5, bottom: 5),
+    return Padding(
+      padding: const EdgeInsets.only(top: 5, bottom: 5),
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
-        child: Row(children: [
-          ProductImage(),
-          ProductImage(
-            rndSeed: 2,
-            nama: "lukisan 1",
-            harga: 5000000,
-          ),
-          ProductImage(
-            rndSeed: 3,
-          )
-        ]),
+        child: Row(
+          children: [
+            ProductImage(),
+            ProductImage(
+              rndSeed: 2,
+              nama: "lukisan 1",
+              harga: 5000000,
+            ),
+            ProductImage(
+              rndSeed: 3,
+            )
+          ],
+        ),
       ),
     );
   }
@@ -77,7 +115,7 @@ class ProductImage extends StatelessWidget {
   final String nama;
   final double harga;
   const ProductImage(
-      {super.key,
+      {Key? key,
       this.rndSeed = 1,
       this.nama = "Product Name",
       this.harga = 0.0});
